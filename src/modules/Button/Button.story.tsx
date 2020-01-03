73ca51929438a9398f03182116479a7892d796dd
Nuno Oliveira <nuno.oliveira@ymetis.com>
2020-01-03 18:02:02 +0000

    prettier

--- src/modules/Button/Button.story.tsx
+++ src/modules/Button/Button.story.tsx
@@ -3,13 +3,17 @@ import BaseButton from "./BaseButton";
 import { Theme } from "../../types";
 import { Icon } from "../Icon/Icon";
 
-export const DerivedButton: React.FC = ({ children, ...props}) => {
-    const AlteredBaseButton = BaseButton.extendStyles((theme: Theme) => ({
-        button: {
-            border: "2px solid blue"
-        }
-    }))
-    const renderLeftIcon = () => <Icon name="add" />;
+export const DerivedButton: React.FC = ({ children, ...props }) => {
+  const AlteredBaseButton = BaseButton.extendStyles((theme: Theme) => ({
+    button: {
+      border: "2px solid blue"
+    }
+  }));
+  const renderLeftIcon = () => <Icon name="add" />;
 
-    return <AlteredBaseButton renderLeftIcon={renderLeftIcon} {...props}>{children}</AlteredBaseButton>
-}
+  return (
+    <AlteredBaseButton renderLeftIcon={renderLeftIcon} {...props}>
+      {children}
+    </AlteredBaseButton>
+  );
+};
