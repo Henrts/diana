f1e35c1b0f07a07d8c34ed08c056be3b1726ca82
Henrique Sousa <henriqmteixeira@gmail.com>
2020-01-03 14:16:38 +0000

    Exported registerTheme

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { BaseButton, H1, H2, H3 } from "./components";
-import initDefaultTheme from "./setup";
+import initDefaultTheme, { registerTheme, changeTheme } from "./setup";
 
-export { BaseButton, H1, H2, H3, initDefaultTheme };
+export { BaseButton, H1, H2, H3, initDefaultTheme, registerTheme, changeTheme };
