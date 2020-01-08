1da094bdd3d8456d2380ec41588404db5c2eee07
Henrique Sousa <henriqmteixeira@gmail.com>
2020-01-08 16:05:25 +0000

    Different config on types file

---
+++ src/setup.d.ts
@@ -0,0 +1,4 @@
+import { Aesthetic } from "aesthetic";
+export declare function initDefaultTheme(): Aesthetic;
+export declare function registerTheme(themeObj?: any): Aesthetic;
+export declare function changeTheme(theme: string): void;
--- ydesign.d.ts
+++ ydesign.d.ts
@@ -1,9 +1,15 @@
 declare namespace ydesign {
-
+    export const H1: React.FC;
+    export const H2: React.FC;
 }
-export * from "./src/modules/Button";
-export * from "./src/modules/Icon";
-export * from "./src/modules/Typography";
-export * from "./src/modules/Chip";
-export * from "./src/setup";
+export const BaseButton: React.FC;
+// export function initDefaultTheme();
+export const registerTheme: React.FC;
+export const changeTheme: React.FC;
+// export function changeTheme(themeName: string);
+
+// export * from "./src/modules/Button";
+// export * from "./src/modules/Icon";
+// export * from "./src/modules/Typography";
+// export * from "./src/modules/Chip";
 export default ydesign;
