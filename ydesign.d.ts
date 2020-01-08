08e2bccb70441e9e1fdf9a8f16a9fdb09d4e18ec
Henrique Sousa <henriqmteixeira@gmail.com>
2020-01-08 15:25:53 +0000

    Updated definitions file

--- ydesign.d.ts
+++ ydesign.d.ts
@@ -1,3 +1,9 @@
 declare namespace ydesign {
+
 }
+export * from "./modules/Button";
+export * from "./modules/Icon";
+export * from "./modules/Typography";
+export * from "./modules/Chip";
+export * from "./setup";
 export default ydesign;
