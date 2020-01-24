054f39144b94b542c09b837fd9a91a99289d38e2
Henrique Sousa <henriqmteixeira@gmail.com>
2020-01-24 11:53:17 +0000

    Name refactoring

---
+++ .storybook/ytheme.js
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  barBg: "black",
+  appBg: "black",
+  appContentBg: "black",
+  brandTitle: "Diana",
+  brandUrl: "https://example.com",
+  brandImage:
+    "https://cdn.shopify.com/s/files/1/1551/9675/products/001-SCRO3420017-diana-of-gabii-statue-large-roman-goddess-of-hunting_1024x1024.jpg?v=1574936075"
+});
