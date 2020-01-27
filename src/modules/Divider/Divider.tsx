05358c1f9c85f3e8589b8cd08309da437752bf4d
Diogo Mendes <46319073+diogo-mendes@users.noreply.github.com>
2020-01-27 15:56:52 +0000

    Add Divider module

---
+++ src/modules/Divider/Divider.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { WithStylesProps, ThemeStyleSheetFactory } from "../../types";
+import { withStyles } from "../../base";
+
+type orientations = "horizontal" | "vertical";
+
+export interface IProps {
+  className: string;
+  orientation: orientations;
+}
+
+const styleSheet: ThemeStyleSheetFactory = theme => ({
+  divider: {
+    backgroundColor: theme.colors.black,
+    "@selectors": {
+      "&.horizontal": {
+        height: "1px",
+        margin: `${theme.spaceUnit.md} 0`,
+        width: "100%"
+      },
+      "&.vertical": {
+        height: "100%",
+        margin: `0 ${theme.spaceUnit.md}`,
+        width: "1px"
+      }
+    }
+  }
+});
+
+const Divider: React.FC<IProps & WithStylesProps> = ({
+  className,
+  cx,
+  orientation = "horizontal",
+  styles
+}) => {
+  const dividerStyles = cx(
+    styles.divider,
+    orientation === "horizontal" && "horizontal",
+    orientation === "vertical" && "vertical",
+    className
+  );
+
+  return <div className={dividerStyles} />;
+};
+
+export default withStyles(styleSheet)(Divider);
