668307cd34ee0da8817704ff4e08386590b9e5c9
Diogo Mendes <diogo.mendes@ymetis.com>
2020-02-11 11:33:17 +0000

    Refactor Divider module

--- src/modules/Divider/Divider.tsx
+++ src/modules/Divider/Divider.tsx
@@ -5,8 +5,8 @@ import { withStyles } from "../../base";
 type orientations = "horizontal" | "vertical";
 
 export interface IProps {
-  className: string;
-  orientation: orientations;
+  className?: string;
+  orientation?: orientations;
 }
 
 const styleSheet: ThemeStyleSheetFactory = theme => ({
