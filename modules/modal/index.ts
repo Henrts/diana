5fa9e8ff1ec6f6ed9f3acd79ce019415b6b73011
migue1coel6odev <50216470+migue1coel6odev@users.noreply.github.com>
2020-02-14 15:09:45 +0000

    Fixed some falty package.json (#20)

--- modules/modal/index.ts
+++ modules/modal/index.ts
@@ -4,4 +4,5 @@ export {
   ModalSetAppElement
 } from "./Modal";
 export { default as ModalHeader } from "./ModalHeader";
+export { default as ModalBody } from "./ModalBody";
 export { default as ModalFooter } from "./ModalFooter";
