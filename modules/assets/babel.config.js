f70d5ea3b1561d5b94b1203b90513fcb9811cb24
migue1coel6odev <50216470+migue1coel6odev@users.noreply.github.com>
2020-02-19 17:46:48 +0000

    refactor(package.json): delete unecessary packages | fix storybook (#26)

--- modules/assets/babel.config.js
+++ modules/assets/babel.config.js
@@ -27,12 +27,7 @@ const presets = [
   [
     "@babel/env",
     {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1"
-      }
+      modules: false
     }
   ],
   "@babel/preset-react"
