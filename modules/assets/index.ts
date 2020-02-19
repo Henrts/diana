d43f51a75a1c6180bfc023aa4a1f673e4fa4a0ab
migue1coel6odev <50216470+migue1coel6odev@users.noreply.github.com>
2020-02-19 13:36:17 +0000

    fix(pck/assets): added babel to generate svg components (#25)

--- modules/assets/index.ts
+++ modules/assets/index.ts
@@ -1,10 +1,15 @@
+/**
+ * This file was auto-generated! Please do NOT edit directly.
+ * Use yarn generate:icons
+ */
+
 /* eslint-disable */
 /// <reference path="./custom.d.ts" />
 /* eslint-enable */
 
 export { default as Add } from "./icons/add.svg";
-export { default as Arrow } from "./icons/arrow.svg";
 export { default as ArrowDown } from "./icons/arrow-down.svg";
+export { default as Arrow } from "./icons/arrow.svg";
 export { default as Block } from "./icons/block.svg";
 export { default as Chart } from "./icons/chart.svg";
 export { default as Check } from "./icons/check.svg";
