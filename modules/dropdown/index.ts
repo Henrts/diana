71b8f9e5bd8f9eb634edec46b2858ac20cd33afe
miguelcoelhodev <miguelcoelhodev@gmail.com>
2020-03-02 10:35:36 +0000

    feat(multidropdown.tsx): added some logical to select all button

--- modules/dropdown/index.ts
+++ modules/dropdown/index.ts
@@ -10,5 +10,17 @@ export {
 } from "./FiltersMultipleDropdown";
 export {
   default as MultipleDropdown,
+<<<<<<< HEAD
   ISimpleMultipleDropdownProps as IMultipleDropdownProps
+=======
+  IMultipleProps as IMultipleDropdownProps
+} from "./BaseMultipleDropdown";
+export {
+  default as FilterMultipleDropdown,
+  IFilterMultipleDropDownProps
+} from "./FiltersMultipleDropdown";
+export {
+  default as SimpleMultipleDropdown,
+  ISimpleMultipleDropDownProps
+>>>>>>> feat(multidropdown.tsx): added some logical to select all button
 } from "./SimpleMultipleDropdown";
