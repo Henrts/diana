91dd23d6b87af178b28309a63888d275ed15fc48
miguelcoelhodev <miguelcoelhodev@gmail.com>
2020-03-10 17:43:20 +0000

    Fix up

--- modules/dropdown/index.ts
+++ modules/dropdown/index.ts
@@ -10,17 +10,5 @@ export {
 } from "./FiltersMultipleDropdown";
 export {
   default as MultipleDropdown,
-<<<<<<< HEAD
   ISimpleMultipleDropdownProps as IMultipleDropdownProps
-=======
-  IMultipleProps as IMultipleDropdownProps
-} from "./BaseMultipleDropdown";
-export {
-  default as FilterMultipleDropdown,
-  IFilterMultipleDropDownProps
-} from "./FiltersMultipleDropdown";
-export {
-  default as SimpleMultipleDropdown,
-  ISimpleMultipleDropDownProps
->>>>>>> feat(multidropdown.tsx): added some logical to select all button
 } from "./SimpleMultipleDropdown";
