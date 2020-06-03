09632d0e29b0b2e437ed719618b2a437d6368dff
Nuno Oliveira <nuno.oliveira@ymetis.com>
2020-06-03 12:22:22 +0100

    fix ModalHeader

--- modules/modal/Modal.tsx
+++ modules/modal/Modal.tsx
@@ -68,7 +68,7 @@ const Modal: React.FC<IAllProps> = ({
       {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
       {React.Children.map(children, (child: any) =>
         React.cloneElement(child, {
-          ...(child.type.displayName === "ModalHeader"
+          ...(child.type.displayName === "ModalHeader" || child.type.name === "ModalHeader"
             ? {
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 onClose: (e: any) => onClose(e)
