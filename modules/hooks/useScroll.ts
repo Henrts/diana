57646716e476de5fc9e175c7a4ae6242590f96e5
Afonso Caldas <afonso.caldas@ymetis.com>
2020-06-05 16:39:17 +0100

    Added useScroll hook

--- modules/hooks/useScroll.ts
+++ modules/hooks/useScroll.ts
@@ -1,1 +1,33 @@
-export const useScroll: {} = () => {};
+import { useState, useEffect } from "react";
+
+export function useScroll(element?: Element) {
+  const scrollingElement = element || document.getElementById("root") || document.body;
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [scrollY, setScrollY] = useState(scrollingElement.scrollTop);
+  const [scrollDirection, setScrollDirection] = useState<string>();
+  const hasScrolledToBottom =
+    scrollY + scrollingElement.clientHeight === scrollingElement.scrollHeight;
+
+  const listener = () => {
+    const { scrollTop } = scrollingElement;
+    setScrollY(scrollTop);
+    setScrollDirection(lastScrollTop < scrollTop ? "down" : "up");
+    setLastScrollTop(scrollTop);
+  };
+
+  useEffect(() => {
+    scrollingElement.addEventListener("scroll", listener);
+    return () => {
+      scrollingElement.removeEventListener("scroll", listener);
+    };
+  });
+
+  return {
+    hasScrolledToBottom,
+    scrollY,
+    scrollDirection,
+    scrollingElement
+  };
+}
+
+export default useScroll;
