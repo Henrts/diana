4f1393208aeac89fa4bbc7dc6b6b2cedea20e5c2
migue1coel6odev <miguelcoelhodev@gmail.com>
2020-08-28 14:07:10 +0100

    chore(storybook): upgrade to latest storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,96 +3,24 @@ const path = require("path");
 const createCompiler = require("@storybook/addon-docs/mdx-compiler-plugin");
 
 module.exports = {
-  stories: ["../modules/**/*.story.(tsx|mdx)", "../stories/**/*.story.(tsx|mdx)"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links", "@storybook/addon-docs/register"],
-  webpackFinal: async config => {
-    config.resolve = {
-      extensions: [".tsx", ".ts", ".js", ".json"]
-    };
-    config.module.rules.push({
-      oneOf: [
-        {
-          test: /\.s?css$/,
-          use: ["style-loader", "css-loader", "sass-loader"]
-        },
-        {
-          test: /\.(stories|story)\.mdx$/,
-          use: [
-            {
-              loader: "babel-loader",
-              // may or may not need this line depending on your app's setup
-              options: {
-                plugins: ["@babel/plugin-transform-react-jsx"]
-              }
-            },
-            {
-              loader: "@mdx-js/loader",
-              options: {
-                compilers: [createCompiler({})]
-              }
-            }
-          ]
-        },
-        {
-          test: /\.(ts|tsx)$/,
-          use: [
-            {
-              loader: path.resolve("./.storybook/typesLoader.js")
-            },
-            {
-              loader: require.resolve("awesome-typescript-loader")
-            },
-            // Optional
-            {
-              loader: require.resolve("react-docgen-typescript-loader")
-            }
-          ]
-        },
-        {
-          test: /\.md.storybook$/,
-          use: "raw-loader"
-        },
-        {
-          // Exclude `js` files to keep "css" loader working as it injects
-          // its runtime that would otherwise processed through "file" loader.
-          // Also exclude `html` and `json` extensions so they get processed
-          // by webpacks internal loaders.
-          exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.ejs$/, /\.json$/, /\.(svg)$/],
-          loader: require.resolve("file-loader"),
-          options: {
-            name: "static/media/[name].[hash:8].[ext]"
-          }
-        }
-      ]
-    });
-    // Removes old SVG Loader from Storybook webpack
-    config.module.rules = config.module.rules.map(rule => {
-      if (rule.test && rule.test.toString().includes("svg")) {
-        const test = rule.test.toString().replace("svg|", "").replace(/\//g, "");
-        return { ...rule, test: new RegExp(test) };
-      } else {
-        return rule;
+  stories: ["../modules/button/*.story.mdx"],
+  addons: [
+    "@storybook/addon-actions",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
       }
-    });
-    // Adds new SVG Loader
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  removeViewBox: false
-                }
-              ]
-            }
-          }
-        }
-      ]
-    });
-
-    return config;
+    },
+    "@storybook/addon-links"
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: prop => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true)
+    }
   }
 };
